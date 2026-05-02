e5b0467c054a4885b38ac765fc4af4b845dc2ef3
Mhdvk <muhammadmahdavikia78@gmail.com>
2026-05-02 19:24:00 +0700

    feat/landing-page

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const Color primaryDarkBlue = Color(0xFF1A3A6B);
+  static const Color primaryMediumBlue = Color(0xFF2563EB);
+  static const Color primaryLightBlue = Color(0xFFDBEAFE);
+
+  // Background
+  static const Color bgLightGray = Color(0xFFF0F4F8);
+  static const Color bgInput = Color(0xFFF1F5F9);
+  static const Color bgInfoBlue = Color(0xFFEFF6FF);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // Text
+  static const Color textDark = Color(0xFF1E293B);
+  static const Color textGray = Color(0xFF64748B);
+  static const Color textLightGray = Color(0xFF94A3B8);
+
+  // Border
+  static const Color borderGray = Color(0xFFE2E8F0);
+
+  // Risk
+  static const Color riskHigh = Color(0xFFEF4444);
+  static const Color riskHighBg = Color(0xFFFEF2F2);
+  static const Color riskMedium = Color(0xFFF59E0B);
+  static const Color riskMediumBg = Color(0xFFFFFBEB);
+  static const Color riskLow = Color(0xFF10B981);
+  static const Color riskLowBg = Color(0xFFECFDF5);
+}
